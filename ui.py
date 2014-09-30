2f566a3507c778ad445dfb4fdfb4b1af38b01b3a
WuDan <wudanxxx@gmail.com>
2014-09-30 16:47:11 +0800

    scroll bar

--- ui.py
+++ ui.py
@@ -8,16 +8,37 @@ class App:
         self.initUI(master)
     
     def initUI(self,master):
+        #create frame
+        frame = Frame(master)
+        frame.grid_rowconfigure(0, weight=1)
+        frame.grid_columnconfigure(0, weight=1)
+        
+        #define scroll bar
+        xscrollbar = Scrollbar(frame, orient=HORIZONTAL)
+        xscrollbar.grid(row=1, column=0, sticky=E+W)
+        
+        yscrollbar = Scrollbar(frame)
+        yscrollbar.grid(row=0, column=1, sticky=N+S)
+        
         #load image use PIL library
         img = Image.open("project.jpeg")
         angle = 180
         tkImage = ImageTk.PhotoImage(img.rotate(180))
-        
+
         #create canvas to load the image
-        canvas = Canvas(master, width = img.size[0]+20 , height = img.size[1]+20)
+        canvas = Canvas(frame, width = img.size[0]+20 , height = img.size[1]+20,xscrollcommand=xscrollbar.set,yscrollcommand=yscrollbar.set)
         canvas.create_image(10, 10,anchor=NW, image=tkImage)
         canvas.image = tkImage      #hold a reference
-        canvas.pack(fill = BOTH, expand = 1)
+        #canvas.pack(fill = BOTH, expand = 1)
+        canvas.grid(row=0, column=0, sticky=N+S+E+W)
+
+        
+        #config scroll bar
+        canvas.config(scrollregion=canvas.bbox(ALL))
+        xscrollbar.config(command=canvas.xview)
+        yscrollbar.config(command=canvas.yview)
+
+        frame.pack()
 
 
 
