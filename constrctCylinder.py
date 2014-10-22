251a795f456fbcca41bf8dbfeb5b8594b52d5589
WuPei <henry.wupei@gmail.com>
2014-10-22 15:36:27 +0800

    added rgb value to the points

--- constrctCylinder.py
+++ constrctCylinder.py
@@ -18,19 +18,19 @@ offset_v = 400
 #draw the bottom circle
 for i in range(n):
 	theta = i*unit_radian
-	point = "{0},{1},{2}\n".format(radius*math.cos(theta)+offset_u,radius*math.sin(theta)+offset_v,0)
+	point = "{0},{1},{2},{r},{g},{b}\n".format(radius*math.cos(theta)+offset_u,radius*math.sin(theta)+offset_v,0,r='255',g='255',b='255')
 	file.write(point)
 
 #draw the middle part(mesh)
 for i in range(m):
 	for j in range(n):
 		theta = j*unit_radian
-		point1 = "{0},{1},{2}\n".format(radius*math.cos(theta)+offset_u,radius*math.sin(theta)+offset_v,i*unitHeight)
+		point1 = "{0},{1},{2},{r},{g},{b}\n".format(radius*math.cos(theta)+offset_u,radius*math.sin(theta)+offset_v,i*unitHeight,r='255',g='255',b='255')
 		file.write(point1)
 
 for i in range(n):
 	theta = i*unit_radian
-	point = "{0},{1},{2}\n".format(radius*math.cos(theta)+offset_u,radius*math.sin(theta)+offset_v,height)
+	point = "{0},{1},{2},{r},{g},{b}\n".format(radius*math.cos(theta)+offset_u,radius*math.sin(theta)+offset_v,height,r='255',g='255',b='255')
 	file.write(point)
 
 file.close()
